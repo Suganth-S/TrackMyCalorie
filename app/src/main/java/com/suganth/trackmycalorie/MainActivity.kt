0f470563be2cfb198bdb4e320395c7fa0de7d83e
Suganth S <suganthsece86@gmail.com>
2022-02-20 13:32:46 +0530

    navigation commit

--- app/src/main/java/com/suganth/trackmycalorie/MainActivity.kt
+++ app/src/main/java/com/suganth/trackmycalorie/MainActivity.kt
@@ -2,17 +2,33 @@ package com.suganth.trackmycalorie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.suganth.trackmycalorie.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        //Actually  we have five fragment and among them we want to show only three run , statistics and settings
+        //fragment , the easiest way to solve this problem is to use the on destination change listener to our navHostFragment
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id){
+                R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                    bottomNavigationView.visibility = View.VISIBLE
+                else -> bottomNavigationView.visibility = View.GONE
+            }
+        }
     }
 }   
